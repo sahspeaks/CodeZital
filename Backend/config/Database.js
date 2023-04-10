168c9be253c8c378da9c43023a58814d2eda7ebf
Abhishek Kumar <akroy2456@gmail.com>
2023-04-10 07:36:49 +0530

    db link modified

--- Backend/config/Database.js
+++ Backend/config/Database.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-  const { connection } = await mongoose.connect("mongodb+srv://sah_speaks:Abhisheksvce@cluster0.teyuzuc.mongodb.net/?retryWrites=true&w=majority");
+  const { connection } = await mongoose.connect("mongodb+srv://sah_speaks:Abhisheksvce@cluster0.teyuzuc.mongodb.net/");
   console.log(`mongodb connected with ${connection.host}`);
 };
 // mongodb://localhost:27017/CodeZitalDB
